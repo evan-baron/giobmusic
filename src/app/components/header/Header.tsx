bf32bcc32040457408931ddbc4011e03e5c6d104
evan-baron <egbaron@gmail.com>
2025-08-08 11:31:36 -0600

    added in new hook to allow for scroll behavior

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,30 +1,41 @@
+'use client';
+
 // Library imports
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 // Styles imports
 import styles from './header.module.scss';
 
+// Hooks imports
+import { useScrollValue } from './useScrollValue';
+
 function Header() {
+	const headerHeight = useScrollValue('10rem', '6rem');
+	const logoHeight = useScrollValue('7.5rem', '6rem');
+	const logoWidth = useScrollValue('17.5rem', '14rem');
+	const navFontSize = useScrollValue('1.75rem', '1.25rem');
+
 	return (
-		<div className={styles['header-wrapper']}>
+		<div className={styles['header-wrapper']} style={{ height: headerHeight }}>
 			<header className={styles.header}>
 				<nav>
-					<ul>
-						<li>
-							<Link href='#top'>
-								<div className={styles.logo}>
-									<Image
-										src='/logo.png'
-										alt='Logo'
-										fill
-										className={styles.image}
-										sizes='(max-width: 768px) 20vw, (max-width: 1200px) 15vw, 10rem'
-									/>
-								</div>
-							</Link>
-						</li>
+					<Link href='#top'>
+						<div
+							className={styles.logo}
+							style={{ height: logoHeight, width: logoWidth }}
+						>
+							<Image
+								src='/logo-white.webp'
+								alt='Logo'
+								fill
+								className={styles.image}
+								sizes='(max-width: 768px) 20vw, (max-width: 1200px) 15vw, 10rem'
+							/>
+						</div>
+					</Link>
+					<ul style={{ fontSize: navFontSize }}>
 						<li>
 							<Link href='#bio'>About</Link>
 						</li>
