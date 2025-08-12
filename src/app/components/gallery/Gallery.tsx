8cb3df91067743892c8bff0ef1f96eece516c4c1
evan-baron <egbaron@gmail.com>
2025-08-12 11:21:06 -0600

    made carousel in gallery need to abstract to its own component now

--- src/app/components/gallery/Gallery.tsx
+++ src/app/components/gallery/Gallery.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 // Library imports
-import React from 'react';
+import React, { useState, useEffect, useMemo, useRef } from 'react';
 import Image from 'next/image';
 
 // Hooks imports
@@ -11,18 +13,219 @@ import styles from './gallery.module.scss';
 
 // Context imports
 
+// Photos
+const photos = [
+	'/gallery/piano-stage.jpg',
+	'/gallery/guitar-building.jpg',
+	'/gallery/piano-dancing.jpeg',
+	'/gallery/guitar-corner.jpg',
+	'/gallery/piano-dark.jpg',
+	'/gallery/guitar-duo.jpg',
+	'/gallery/piano-duet.jpg',
+	'/gallery/guitar-outside.jpg',
+	'/gallery/piano-front.jpg',
+	'/gallery/guitar-stage.jpg',
+	'/gallery/piano-outside.jpg',
+	'/gallery/guitar-tux.jpg',
+	'/gallery/piano-pov.jpg',
+	'/gallery/piano-suspenders.jpg',
+];
+
 const Gallery = () => {
+	// Extra photos[0] added at end to make length odd for centering purposes
+	const carouselPhotos = [...photos, ...photos, photos[0]];
+
+	const [currentIndex, setCurrentIndex] = useState(photos.length);
+	const [imageDimension, setImageDimension] = useState(140);
+	const [transformDist, setTransformDist] = useState(0);
+	const [isTransitioning, setIsTransitioning] = useState(false);
+	const [clickDisabled, setClickDisabled] = useState(false);
+
+	const carouselRef = useRef<HTMLDivElement>(null);
+	const carouselWidth = carouselRef.current?.offsetWidth;
+	const desiredImageWidth = 140; // Width of each image in pixels
+	const GAP = 16; // Gap between images in pixels
+	const transitionTime = 0.4; // Transition time in seconds
+	const shift = (imageDimension + GAP) / 16;
+
+	useEffect(() => {
+		if (carouselRef.current) {
+			// Calculating how many images can fit in the carousel track
+			const length = Math.floor(
+				carouselRef.current.offsetWidth / (desiredImageWidth + GAP)
+			);
+
+			// If the length is even, we add an extra image to the carousel to keep the number odd so indexed image is always in the center
+			if (length % 2 === 0) {
+				setImageDimension(
+					(carouselRef.current.offsetWidth - GAP * (length + 1)) / (length + 1)
+				);
+			} else {
+				setImageDimension(
+					(carouselRef.current.offsetWidth - 16 * (length - 1)) / length
+				);
+			}
+		}
+
+		const resizeCarousel = () => {
+			if (carouselRef.current) {
+				const length = Math.floor(
+					carouselRef.current.offsetWidth / (desiredImageWidth + GAP)
+				);
+
+				if (length % 2 === 0) {
+					setImageDimension(
+						(carouselRef.current.offsetWidth - GAP * (length + 1)) /
+							(length + 1)
+					);
+				} else {
+					setImageDimension(
+						(carouselRef.current.offsetWidth - GAP * (length - 1)) / length
+					);
+				}
+			}
+		};
+
+		window.addEventListener('resize', resizeCarousel);
+
+		return () => {
+			window.removeEventListener('resize', resizeCarousel);
+		};
+	}, [carouselWidth, currentIndex]);
+
+	const handleScroll = (direction: string) => {
+		if (clickDisabled) return;
+
+		if (carouselRef.current) {
+			if (direction === 'left') {
+				setIsTransitioning(true);
+				setTransformDist((prev) => prev + shift);
+
+				if (currentIndex - 1 < photos.length / 2) {
+					setCurrentIndex((prev: number) => prev - 1 + photos.length);
+					setClickDisabled(true);
+					setTimeout(() => {
+						setIsTransitioning(false);
+						setTransformDist((prev) => prev * -1 + shift * 2);
+						setClickDisabled(false);
+					}, transitionTime * 1000);
+				} else {
+					setCurrentIndex((prev: number) => prev - 1);
+				}
+			} else if (direction === 'right') {
+				setIsTransitioning(true);
+				setTransformDist((prev) => prev - shift);
+
+				if (currentIndex + 1 > photos.length * 1.5) {
+					setCurrentIndex((prev: number) => prev + 1 - photos.length);
+					setClickDisabled(true);
+
+					setTimeout(() => {
+						setIsTransitioning(false);
+						setTransformDist((prev) => prev * -1 - shift * 2);
+						setClickDisabled(false);
+					}, transitionTime * 1000);
+				} else {
+					setCurrentIndex((prev: number) => prev + 1);
+				}
+			}
+		}
+	};
+
+	const handleClick = (index: number) => {
+		if (clickDisabled) return;
+
+		setIsTransitioning(true);
+
+		const isOuterLimitLeft = index < photos.length / 2;
+		const isOuterLimitRight = index > photos.length * 1.5;
+
+		// Determining new index if outer limits are crossed or not
+		const newIndex = () => {
+			if (isOuterLimitLeft) {
+				return index + photos.length;
+			} else if (isOuterLimitRight) {
+				return index - photos.length;
+			}
+			return index;
+		};
+
+		// Calculate the change in transform distance in rem
+		const transformChange = (newIndex() - currentIndex) * shift;
+
+		// Default change
+		const defaultChange = (index - currentIndex) * shift;
+
+		// Update the current index and transform distance
+		if (isOuterLimitLeft || isOuterLimitRight) {
+			setTransformDist((prev) => prev - defaultChange);
+			setClickDisabled(true);
+
+			setTimeout(() => {
+				setIsTransitioning(false);
+				setTransformDist((prev) => {
+					return prev - transformChange + defaultChange;
+				});
+				setCurrentIndex(newIndex());
+				setClickDisabled(false);
+			}, transitionTime * 1000);
+		} else {
+			setCurrentIndex(newIndex());
+			setTransformDist((prev) => prev - transformChange);
+		}
+	};
+
 	return (
 		<div className={styles['gallery-wrapper']}>
 			<div className={styles.gallery}>
 				<div className={styles.content}>
 					<h2>Gallery</h2>
-					<div className={styles.carousel}>
-						<div className={styles.left}></div>
-						<div className={styles.image}></div>
-						<div className={styles.right}></div>
+					<div className={styles['gallery-image']}>
+						<div
+							className={styles.left}
+							onClick={() => handleScroll('left')}
+						></div>
+						<div className={styles.image}>
+							<Image
+								src={carouselPhotos[currentIndex]}
+								alt='Gallery image'
+								fill
+								style={{ objectFit: 'cover', borderRadius: '1rem' }}
+							/>
+						</div>
+						<div
+							className={styles.right}
+							onClick={() => handleScroll('right')}
+						></div>
 					</div>
-					<div className={styles['carousel-select']}></div>
+					<div className={styles['carousel']} ref={carouselRef}>
+						<div
+							className={styles['carousel-track']}
+							style={{
+								gap: `${GAP / 16}rem`,
+								transform: `translateX(${transformDist}rem)`,
+								transition: isTransitioning
+									? `transform ${transitionTime}s`
+									: 'none',
+							}}
+						>
+							{carouselPhotos.map((img, idx) => (
+								<div
+									className={styles['carousel-image']}
+									key={idx}
+									onClick={() => handleClick(idx)}
+								>
+									<Image
+										src={img}
+										alt={`Gallery image ${idx + 1}`}
+										width={imageDimension}
+										height={imageDimension}
+										style={{ objectFit: 'cover', borderRadius: '1rem' }}
+									/>
+								</div>
+							))}
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
